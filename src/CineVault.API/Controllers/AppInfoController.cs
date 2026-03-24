48afc4a9806aefb8556117116febc8e88598c2d5
KuksaAnastasia <229111@kisit.kneu.edu.ua>
2026-03-24 13:05:32 +0200

    Add lab7 environment settings

---
+++ src/CineVault.API/Controllers/AppInfoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CineVault.API.Controllers;
+
+[ApiController]
+[Route("api")]
+public class AppInfoController : ControllerBase
+{
+    private readonly IHostEnvironment _environment;
+
+    public AppInfoController(IHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
+    [HttpGet("environment")]
+    public IActionResult GetEnvironment()
+    {
+        return Ok(new
+        {
+            Environment = _environment.EnvironmentName,
+            // Додаткова логіка для перевірки
+            IsDevelopment = _environment.IsDevelopment(),
+            IsLocal = _environment.IsEnvironment("Local")
+        });
+    }
+}
---
+++ src/CineVault.API/Extensions/IHostEnvironmentExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace CineVault.API.Extensions;
+
+// Клас розширень для IHostEnvironment
+public static class IHostEnvironmentExtensions
+{
+    // Метод розширення IsLocal() перевіряє, чи середовище виконання локальне
+    public static bool IsLocal(this IHostEnvironment env)
+        => env.EnvironmentName.Equals("Local", StringComparison.OrdinalIgnoreCase);
+}
