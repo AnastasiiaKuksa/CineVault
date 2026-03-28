1dc11b08559f23e7a90c5ff076068d8c6dad454e
KuksaAnastasia <229111@kisit.kneu.edu.ua>
2026-03-29 00:42:26 +0200

    Add background services for statistics and old movies cleanup

---
+++ src/CineVault.API/Migrations/20260328220359_AddMovieStatistics.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineVault.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieStatistics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovieStatistics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    AverageRating = table.Column<double>(type: "float(5)", precision: 5, scale: 2, nullable: false),
+                    ReviewsCount = table.Column<int>(type: "int", nullable: false),
+                    CommentsCount = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieStatistics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovieStatistics_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieStatistics_MovieId",
+                table: "MovieStatistics",
+                column: "MovieId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieStatistics");
+        }
+    }
+}
